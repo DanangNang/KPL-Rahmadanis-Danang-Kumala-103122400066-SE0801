f85fc1e537480ea4e2f1752192ef84a11f3cb644
DanangNang <faradisaayubanget@gmail.com>
2026-03-05 17:42:06 +0700

    Update Folder TM

---
+++ 02_Pemrograman_JavaScript/TM/test.js
@@ -0,0 +1,6 @@
+const fizzBuzz = require("./tm");
+const input = [8, 9, 32, 75, 84];
+const output = fizzBuzz(input);
+
+console.log("Input :", input);
+console.log("Output :", output);
---
+++ 02_Pemrograman_JavaScript/TM/tm.js
@@ -0,0 +1,20 @@
+function fizzBuzz(arr) {
+    let hasil = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let num = arr[i];
+        
+        if (num % 14 === 0) {
+            hasil.push("FizzBuzz");
+        } else if (num % 2 === 0) {
+            hasil.push("Fizz");
+        } else if (num % 7 === 0) {
+            hasil.push("Buzz");
+        } else {
+            hasil.push(num);
+        }
+    }
+    return hasil.join(" ");
+}
+
+module.exports = fizzBuzz;
