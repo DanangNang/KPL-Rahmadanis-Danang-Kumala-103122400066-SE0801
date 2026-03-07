14ce407abb17b318f52a84610933e1434fc3efda
DanangNang <faradisaayubanget@gmail.com>
2026-03-07 20:38:24 +0700

    KPL03

---
+++ 03_Pengenalan_HTML_&_CSS/TP/Pengenalan_HTML_&_CSS.md
@@ -0,0 +1,5 @@
+HTML (Hypertext Markup Language) adalah bahasa markah (bukan bahasa pemrograman) untuk menyusun dan menampilkan dokumen Web. Dikembangkan oleh Tim Berners-Lee pada 1993, HTML adalah inti teknologi WWW bersama HTTP dan URI/URL. Dulu populer HTML 4.01 dan XHTML 1.0, kini digunakan HTML5.
+
+Spesifikasi HTML sempat menjadi konflik antara W3C dan WHATWG. WHATWG dibentuk pada 2004 karena W3C fokus ke XHTML (revisi HTML4 berbasis XML). Pada 2019, W3C setuju menyerahkan pemeliharaan spesifikasi HTML sepenuhnya kepada WHATWG.
+
+CSS (Cascading Style Sheets) adalah bahasa pengayaan, diusulkan Håkon Wium Lie pada 1994, digunakan untuk mengatur tampilan dokumen Web (HTML) atau SVG. Versi terkini adalah CSS3, dengan
---
+++ 03_Pengenalan_HTML_&_CSS/TP/index.js
@@ -0,0 +1,34 @@
+let textInput = document.getElementById("textInput");
+let total = document.getElementById("total");
+let upper = document.getElementById("upper");
+let lower = document.getElementById("lower");
+
+function countLetters() {
+    let text = textInput.value;
+
+    let totalCount = text.length;
+    let upperCount = 0;
+    let lowerCount = 0;
+
+}
+
+textInput.addEventListener("input", countLetters);
+
+function toUpper(){
+    textInput.value = textInput.value.toUpperCase();
+    countLetters();
+}
+
+function toLower(){
+    textInput.value = textInput.value.toLowerCase();
+    countLetters();
+}
+
+function toCapitalize(){
+    let text = textInput.value.toLowerCase().split(" ");
+    for(let i=0;i<text.length;i++){
+        text[i] = text[i].charAt(0).toUpperCase() + text[i].slice(1);
+    }
+    textInput.value = text.join(" ");
+    countLetters();
+}
