50dcc4481f9a7299331447356087079f2f314c67
DanangNang <faradisaayubanget@gmail.com>
2026-03-30 13:56:20 +0700

    Update TM

---
+++ 04_Automata_Table-Driven_Construction/TM/index3.js
@@ -0,0 +1,56 @@
+let textInput = document.getElementById("textInput");
+let total = document.getElementById("Total");
+let upper = document.getElementById("Upper");
+let lower = document.getElementById("Lower");
+
+function countLetters() {
+    let text = textInput.value;
+
+    let totalCount = text.length;
+    let upperCount = 0;
+    let lowerCount = 0;
+
+    for (let i = 0; i < text.length; i++) {
+        let char = text[i];
+        if (char >= 'A' && char <= 'Z') {
+            upperCount++;
+        }
+        if (char >= 'a' && char <= 'z') {
+            lowerCount++;
+        }
+    }
+    total.textContent = totalCount;
+    upper.textContent = upperCount;
+    lower.textContent = lowerCount;
+}
+textInput.addEventListener("input", countLetters);
+
+function toUpper(){
+    document.getElementById("editor-kecil").value =
+        textInput.value.toUpperCase();
+}
+
+function toLower(){
+    document.getElementById("editor-kecil").value =
+        textInput.value.toLowerCase();
+}
+
+function darkMode() {
+    resetMode();
+    document.body.classList.add("dark-mode");
+}
+
+function lightMode() {
+    resetMode();
+    document.body.classList.remove("dark-mode");
+}
+
+function sepiaMode() {
+    resetMode();
+    document.body.classList.add("sepia-mode");
+}
+
+function resetMode() {
+    document.body.classList.remove("dark-mode");
+    document.body.classList.remove("sepia-mode");
+}
